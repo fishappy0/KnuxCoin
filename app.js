49d1c7cdc583f9e522e06704f9f565f696cdeb74
Fishappy0 <fishappy0@gmail.com>
2022-03-24 01:29:18 +0700

    Thêm phần layout trang user html + CSS, add file todo và làm housekeeping
    Hình ảnh trang layout đã gửi trong group, file todo chứa các việc cần làm (ae chọn thoải mái ko có phân công căng thẳng), houskeeping của ngày: thêm vào library cookie parser và sắp xếp lại file appjs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express =  require('express');
 const app = express();
 const hbars = require('express-handlebars');
+const session = require('express-session');
+const cookie = require('cookie-parser')
 const multiparty = require('multiparty')
+const path = require('path');
 
 const port = 8080;
-
 app.engine('handlebars', hbars.engine({
     defaultLayout: '',
     helpers: {
@@ -12,8 +14,12 @@ app.engine('handlebars', hbars.engine({
     }
 }))
 
-app.set('view engine', 'handlebars')
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname+'/public'))
+
+//Trang landing
 app.get('/', (req, res) => res.render('index'))
 
 //Trang đăng nhập
@@ -28,8 +34,12 @@ app.post('/register', (req, res) =>{
 
 })
 
+//Trang dashboard
+app.get('/dashboard', (req, res) => res.render('user/dashboard', {layout: 'user'}))
+app.post('/dashboard', (req, res) =>{
 
-app.use(express.static(__dirname+'/public'))
+})
+
 
 app.listen(port, ()=> console.log(
     `<KnuxCoin Web> Started on port ${port}` + `\n<KnuxCoin Web> Terminate with [Ctrl] + [C]`
