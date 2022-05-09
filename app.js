63198b9918693b829fdb89baac529304468e17c7
Fishappy0 <fishappy0@gmail.com>
2022-05-10 01:37:53 +0700

    Thêm model kết nối với mongoDB, thêm logic cho post của trang đăng nhập

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express =  require('express');
 const app = express();
 const hbars = require('express-handlebars');
 const session = require('express-session');
-const cookie = require('cookie-parser')
-const multiparty = require('multiparty')
+const cookie = require('cookie-parser');
+const multiparty = require('multiparty');
+const body_parser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const port = 8080;
+const mongoUrl = "mongodb://0.0.0.0:27017/test-db";
+
+const testModel = require('./model/testdb_model.js');
+mongoose.connect(mongoUrl).then(()=>{console.log(`<KnuxCoin Web> Connected to the database at ${mongoUrl}`)});
+var parseBody = body_parser.urlencoded({extended: false});
 app.engine('handlebars', hbars.engine({
     defaultLayout: '',
     helpers: {
 
     }
-}))
+}));
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -24,14 +31,32 @@ app.get('/', (req, res) => res.render('index'))
 
 //Trang đăng nhập
 app.get('/login', (req, res) => res.render('account/login')) 
-app.post('/login', (req, res) =>{
-
+app.post('/login', parseBody,(received, res) => {
+    let req = received.body;
+    let username = req.username;
+    let password = req.password;
+    console.log(`Password is ${password} and username is ${username}`);
+    if(testModel.verifyAccount(username, password, `thisisakeylmao`)){
+        res.redirect('/dashboard');
+    };
 })
 
 //Trang đăng kí
 app.get('/register', (req, res) => res.render('account/register'))
-app.post('/register', (req, res) =>{
-
+app.post('/register', parseBody, (req, res) =>{
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) =>{
+        if(err) return res.status(500).send(err.message);
+        let birthday = fields.birthday;
+        let email = fields.address;
+        let address = fields.mail;
+        let full_name = fields.full_name;
+    
+        console.log(`full name: ${full_name}`)
+        console.log(`mail: ${email}`)
+        console.log(`address: ${address}`)
+        console.log(`birthday: ${birthday}`)
+    })
 })
 
 //Trang dashboard
@@ -39,7 +64,6 @@ app.get('/dashboard', (req, res) => res.render('user/dashboard', {layout: 'user'
 app.post('/dashboard', (req, res) =>{
 
 })
-
 
 app.listen(port, ()=> console.log(
     `<KnuxCoin Web> Started on port ${port}` + `\n<KnuxCoin Web> Terminate with [Ctrl] + [C]`
---
+++ model/testdb_model_user_info.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({});
+const testDB = mongoose.model('test_db_info', {
+    full_name: String,
+    email: String,
+    address: String,
+    dob: String,
+    id_sidea:{
+        data: Buffer,
+        contentType: String
+    },
+    id_sideb: {
+        data: Buffer,
+        contentType: String
+    }
+})
+// var functions = {};
+
+module.exports.verifyAccount = function (uname, passphrase, sKey){
+   if (testDB.findOne({username: uname, password: passphrase, serverKey: sKey}, (err, account) =>{
+       if(err) throw err; 
+   }) != null) return true;
+   return false;
+}
+
+module.exports.createAccount = function (uname, passphrase, sKey){
+    const oneData = new testDB({
+        username:  uname,
+        password: passphrase,
+        serverKey: sKey
+    });
+    oneData.save().then(() => 
+            console.log(`<KnuxCoin Web> Created account for ${uname}`)
+    );
+}
+// [=] connect to the db [=]
+// const mongoose = require('mongoose');
+// const mongoUrl = "mongodb://0.0.0.0:27017/test-db";
+// mongoose.connect(mongoUrl).then(()=>{console.log('connected')});
+
+// [=] load table [=]
+// const userSchema = new mongoose.Schema({});
+// const cat = mongoose.model('cats', {
+//     name: String
+// });
+
+// [=] write data to table [=]
+// const oneData = new cat({
+//     name: 'bede'
+// });
+// oneData.save().then(() => console.log('created'));
+
+// [=] Find data in table [=]
+// cat.findOne({name: 'kat'}, (err, Cat) =>{
+//     if(err) throw err;
+//     console.log(`cat name: ${Cat.name}`)
+// })
+// const 
+// const kitty = new Cat({
+//     name: 'kat'
+// });
+
+// module.exports{
+// }
