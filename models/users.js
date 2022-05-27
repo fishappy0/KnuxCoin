5b3697ae4d960730971892bcb7b6a31618ac7b61
Fishappy0 <fishappy0@gmail.com>
2022-05-27 10:42:43 +0700

    update reformat

--- models/users.js
+++ models/users.js
@@ -1,17 +1,5 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  full_name: String,
-  email: String,
-  address: String,
-  dob: Date,
-  phone: String,
-  createdAt: Date,
-  lockedAt: Date,
-  loginFail: Number,
-  abnormalLogin: Number,
-  status: String,
-=======
     id: Number,
     full_name: String,
     email: String,
@@ -24,36 +12,10 @@ const userSchema = new mongoose.Schema({
     abnormalLogin: Number,
     status: String,
     balance: Number,
->>>>>>> origin/main
 });
 var User = mongoose.model("User", userSchema); //Tạo collection
 module.exports = User;
 
-<<<<<<< HEAD
-module.exports.createAccount = async function (
-  full_name,
-  email,
-  address,
-  dob,
-  phone
-) {
-  let current_time = new Date(Date.now());
-  const oneData = await new User({
-    full_name: full_name,
-    email: email,
-    address: address,
-    dob: dob,
-    phone: phone,
-    createdAt: current_time.toString(),
-    lockedAt: "",
-    loginFail: 0,
-    abnormalLogin: 0,
-    status: "waiting",
-  });
-  await oneData.save();
-  console.log(`<KnuxCoin Web> Created user with phone number ${phone}`);
-};
-=======
 module.exports.createAccount = async function(id, full_name, email, address, dob, phone){
     let current_time = new Date(Date.now())
     const oneData = await new userDB({
@@ -74,4 +36,3 @@ module.exports.createAccount = async function(id, full_name, email, address, dob
     console.log(`<KnuxCoin Web> Created user with phone number ${phone}`);
 }
 
->>>>>>> origin/main
