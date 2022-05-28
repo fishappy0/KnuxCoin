b6630b51ed2263d6a591bb447041d6da6bb60a21
Fishappy0 <fishappy0@gmail.com>
2022-05-28 18:30:22 +0700

    bug fixes after commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,21 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const { dbg } = require("../local_utils");
-<<<<<<< HEAD
-=======
 const alert = require("alert");
->>>>>>> dev
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   sess = req.session;
   if (typeof sess.username != "undefined") {
     if (sess.first_time == true) {
-<<<<<<< HEAD
-      res.render("account/password", { error: "Please change your password before using the system!" })
-    }
-    else res.render("user/dashboard");
-=======
       alert("Please change your password before using the system!");
       res.redirect("/password");
     } else
@@ -23,7 +15,6 @@ router.get("/", function (req, res, next) {
         full_name: req.session.full_name,
         email: req.session.email,
       });
->>>>>>> dev
   } else {
     res.redirect("/");
   }
