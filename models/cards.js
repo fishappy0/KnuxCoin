4dc70e638877ba7597d3141735d3ec06f8d568a8
Đỗ Gia Bảo <62286955+IowaSanae@users.noreply.github.com>
2022-05-29 04:55:40 +0700

    Hoàn thành tính năng rút tiền

--- models/cards.js
+++ models/cards.js
@@ -5,12 +5,12 @@ const Transaction = require('../models/transaction');
 const Wallet = require('../models/wallet');
 const crypto = require('crypto');
 /**
-  * Đọc tài liệu của thầy để biết tại sao có cái này.
-  * CardId = STT
-  * CardNumber = Số
-  * expiryDate = Ngày hết hạn
-  * cvv = Mã CVV
-  **/
+* Schema cho thẻ cào.
+* CardId = STT
+* CardNumber = Số
+* expiryDate = Ngày hết hạn
+* cvv = Mã CVV
+*/
 const cardSchema = new mongoose.Schema({
   cardId: Number,
   cardNumber: Number,
@@ -86,48 +86,46 @@ module.exports.cardThree = async function () {
   return cardThreeDataArray;
 }
 
-// Chức năng nạp tiền.
+/**
+ * Chức năng nạp tiền.
+ * @param {int} cardNumber Số thẻ cào
+ * @param {date} expiryDate Ngày hết hạn
+ * @param {int} cvv Mã CVV
+ * @param {int} amount Số tiền nạp
+ * @returns {string} Thông báo nạp thành công hoặc lỗi.
+ */
 module.exports.recharge = async function (cardNumber, expiryDate, cvv, amount) {
   let cardOneExpiryDate = new Date(2022, 10, 10);
   let cardTwoExpiryDate = new Date(2022, 11, 11);
   let cardThreeExpiryDate = new Date(2022, 12, 12);
+  let transactionId = crypto.randomBytes(16).toString('hex');
 
-  if (cardNumber.length != 6) {
-    return 'Số thẻ không hợp lệ';
-  }
-
-  if (cvv.length != 3) {
-    return 'Mã CVV không hợp lệ';
-  }
+  if (cardNumber.length != 6) return 'Số thẻ không hợp lệ';
+  if (cvv.length != 3) return 'Mã CVV không hợp lệ';
 
   // Thẻ 1 không giới hạn.
   if (cardNumber == 111111) {
-    if (expiryDate.getTime() != cardOneExpiryDate.getTime()) {
-      return 'Thời hạn thẻ không hợp lệ';
-    }
-
-    if (cvv != 411) {
-      return 'Mã CVV không hợp lệ';
-    }
+    if (expiryDate.getTime() != cardOneExpiryDate.getTime()) return 'Thời hạn thẻ không hợp lệ';
+    if (cvv != 411) return 'Mã CVV không hợp lệ';
 
     const user = await User.findOne({ userId: user.id });
     user.balance += amount;
     await user.save();
+    await Transaction.create({
+      userId: user.id,
+      transactionId: transactionId,
+      amount: amount,
+      type: 'recharge',
+      status: 'approved',
+    });
     return 'Nạp tiền thành công';
   }
 
   // Thẻ 2 nạp tối đa 1 triệu/lần.
   if (cardNumber == 222222) {
-    if (expiryDate.getTime() != cardTwoExpiryDate.getTime()) {
-      return 'Thời hạn thẻ không hợp lệ';
-    }
-    if (cvv != 443) {
-      return 'Mã CVV không hợp lệ';
-    }
-
-    if (amount > 1000000) {
-      return 'Không thể nạp quá 1 triệu';
-    }
+    if (expiryDate.getTime() != cardTwoExpiryDate.getTime()) return 'Thời hạn thẻ không hợp lệ';
+    if (cvv != 443) return 'Mã CVV không hợp lệ';
+    if (amount > 1000000) return 'Không thể nạp quá 1 triệu';
 
     const user = await User.findOne({ userId: user.id });
     user.balance += amount;
@@ -137,13 +135,8 @@ module.exports.recharge = async function (cardNumber, expiryDate, cvv, amount) {
 
   // Thẻ 3 luôn hết tiền.
   if (cardNumber == 333333) {
-    if (expiryDate.getTime() != cardThreeExpiryDate.getTime()) {
-      return 'Thời hạn thẻ không hợp lệ';
-    }
-    if (cvv != 577) {
-      return 'Mã CVV không hợp lệ';
-    }
-
+    if (expiryDate.getTime() != cardThreeExpiryDate.getTime()) return 'Thời hạn thẻ không hợp lệ';
+    if (cvv != 577) return 'Mã CVV không hợp lệ';
     return 'Thẻ hết tiền';
   }
 
@@ -153,40 +146,49 @@ module.exports.recharge = async function (cardNumber, expiryDate, cvv, amount) {
 
 
 /**
-  * Chức năng rút tiền
-  * cardNumber = số thẻ (người dùng nhập)
-  * expiryDate = ngày hết hạn (người dùng nhập)
-  * cvv = Mã CVV (người dùng nhập)
-  * description = mô tả (người dùng nhập) nhưng phải ghi nhận vào lịch sử giao dịch
-  * amount = số tiền muốn rút (người dùng nhập)
-  * times = số lần rút (mỗi ngày chỉ được tối 2 lần rút) => Cần có chức năng lịch sử chăng?
-  * @todo: Nếu quá 5 triệu thì phải báo admin.
-  **/
-module.exports.withdraw = async function (cardNumber, expiryDate, cvv, description, amount, times) {
+ * Chức năng rút tiền
+ * @param {int} cardNumber Số thẻ cào
+ * @param {date} expiryDate Ngày hết hạn
+ * @param {int} cvv Mã CVV
+ * @param {string} description Ghi chú
+ * @param {int} amount Số tiền rút
+ * @returns {string} Thông báo rút tiền thành công hoặc lỗi.
+ */
+module.exports.withdraw = async function (cardNumber, expiryDate, cvv, description, amount) {
   // Tính năng mô phỏng => chỉ cần rút từ thẻ đầu tiên nên set cứng luôn.
   let defaultExpiryDate = new Date(2022, 10, 10);
+  let transactionId = crypto.randomBytes(16).toString("hex");
   if (cardNumber == 111111 && expiryDate == defaultExpiryDate && cvv == 411) {
     const user = await User.findOne({ userId: user.id });
-    if (amount > 5000000) {
-      const transactionId = crypto.randomBytes(16).toString("hex");
-      // Chuyển status sang pending.
-      await Transaction.findOneAndUpdate({ transactionId: transactionId }, { status: 'pending' });
-      if (amount > user.balance) {
-        if (amount % 50000 == 0) {
+    // Đếm số lần rút tiền của hôm nay.
+    const count = await Transaction.countDocuments({
+      userId: user.id,
+      date: new Date().getDate(),
+      type: 'withdraw',
+    }).count();
+    if (count > 2) return 'Bạn đã rút quá 2 lần trong ngày';
+    else {
+      if (amount > 5000000) return Transaction.findOneAndUpdate({ transactionId: transactionId }, { status: 'pending' });
+      if (amount > user.balance) return 'Không đủ tiền rút';
+      else {
+        if (amount % 50000 != 0) return 'Số tiền rút mỗi lần phải là bội số của 50,000 đồng.';
+        else {
           // 5% phí rút tiền
           user.balance -= amount * 0.95;
           await user.save();
+          // Ghi nhận vào lịch sử giao dịch
+          await Transaction.create({
+            userId: user.id,
+            transactionId: transactionId,
+            type: 'withdraw',
+            amount: amount,
+            description: description,
+            status: 'approved',
+          });
           return 'Rút tiền thành công';
-        } else {
-          return 'Số tiền rút mỗi lần phải là bội số của 50,000 đồng.';
         }
-      } else {
-        return 'Không đủ tiền rút';
       }
     }
-  } else if (cardNumber == 222222 || cardNumber == 333333) {
-    return 'Thẻ này không được hỗ trợ để rút tiền';
-  } else {
-    return 'Thông tin thẻ không hợp lệ';
-  }
+  } else if (cardNumber == 222222 || cardNumber == 333333) return 'Thẻ này không được hỗ trợ để rút tiền';
+  else return 'Thông tin thẻ không hợp lệ';
 }
